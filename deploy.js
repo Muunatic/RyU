ee9f36d29c152623a2ed571e398ec4e784a2f442
Muunatic <ichigoez0@gmail.com>
2023-03-01 21:42:06 +0700

    style: formatting indent space

--- deploy.js
+++ deploy.js
@@ -3,29 +3,23 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const { token, clientId } = require('./src/data/config.json');
 
-const commands = []
-
-.map(command => command.toJSON());
+const commands = [].map(command => command.toJSON());
 
 const commandFiles = fs.readdirSync(`./src/commands/interaction`).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-	const command = require(`./src/commands/interaction/${file}`);
-	commands.push(command.data.toJSON());
+    const command = require(`./src/commands/interaction/${file}`);
+    commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
-
-	try {
-		console.log('Refreshing...');
-		await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
-		console.log('Deployed!');
-	} catch (error) {
-		console.error(error);
-	}
+    try {
+        console.log("Refreshing...");
+        await rest.put(Routes.applicationCommands(clientId), { body: commands });
+        console.log("Deployed!");
+    } catch (error) {
+        console.error(error);
+    }
 
 })();
