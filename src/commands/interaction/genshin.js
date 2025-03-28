ce48b9bb6ae0ba63b536851bba6cf1ce1a915330
Muunatic <muunatic@typeslint.com>
2025-03-28 10:19:40 +0700

    refactor: remove unnecessary JSON parsing and update property names

--- src/commands/interaction/genshin.js
+++ src/commands/interaction/genshin.js
@@ -22,21 +22,20 @@ module.exports = {
     async execute(interaction) {
         if (interaction.options.get('weapon')) {
             const data = genshindb.weapons(interaction.options.get('weapon').value);
-            const current = data;
-            const moracosts = current.costs.ascend1[0].count + current.costs.ascend2[0].count + current.costs.ascend3[0].count + current.costs.ascend4[0].count + current.costs.ascend5[0].count + current.costs.ascend6[0].count;
+            const moracosts = data.costs.ascend1[0].count + data.costs.ascend2[0].count + data.costs.ascend3[0].count + data.costs.ascend4[0].count + data.costs.ascend5[0].count + data.costs.ascend6[0].count;
 
             const embed = new MessageEmbed()
 
             .setColor('#89e0dc')
-            .setTitle(current.name)
-            .setDescription(current.description)
-            .setThumbnail(current.images.mihoyo_icon)
+            .setTitle(data.name)
+            .setDescription(data.description)
+            .setThumbnail(data.images.mihoyo_icon)
             .addFields(
-                { name: 'Weapon', value: `${current.weaponText}`, inline: true },
-                { name: 'Rarity', value: `${current.rarity}✰`, inline: true },
-                { name: 'Substat', value: `${current.mainStatText}`, inline: true },
-                { name: 'Refine', value: `${current.r1.description}`, inline: true },
-                { name: 'Material', value: `${current.costs.ascend6[0].name + ', ' + current.costs.ascend6[1].name + ', ' + current.costs.ascend6[2].name}`, inline: true },
+                { name: 'Weapon', value: `${data.weaponText}`, inline: true },
+                { name: 'Rarity', value: `${data.rarity}✰`, inline: true },
+                { name: 'Substat', value: `${data.mainStatText}`, inline: true },
+                { name: 'Refine', value: `${data.r1.description}`, inline: true },
+                { name: 'Material', value: `${data.costs.ascend6[0].name + ', ' + data.costs.ascend6[1].name + ', ' + data.costs.ascend6[2].name}`, inline: true },
                 { name: 'Costs', value: `${moracosts}`, inline: true }
             )
             .setFooter({text: `Direquest oleh ${interaction.member.nickname || interaction.user.username}`, iconURL: interaction.user.avatarURL({format : 'png', dynamic : true, size : 1024})})
@@ -44,25 +43,22 @@ module.exports = {
             interaction.reply({embeds: [embed]});
         } else if (interaction.options.get('characters')) {
             const data = genshindb.characters(interaction.options.get('characters').value);
-            const datastringtify = JSON.stringify(data);
-            const dataparse = JSON.parse(datastringtify);
-            const current = dataparse;
-            const moracosts = current.costs.ascend1[0].count + current.costs.ascend2[0].count + current.costs.ascend3[0].count + current.costs.ascend4[0].count + current.costs.ascend5[0].count + current.costs.ascend6[0].count;
+            const moracosts = data.costs.ascend1[0].count + data.costs.ascend2[0].count + data.costs.ascend3[0].count + data.costs.ascend4[0].count + data.costs.ascend5[0].count + data.costs.ascend6[0].count;
 
             const embed = new MessageEmbed()
 
             .setColor('#89e0dc')
-            .setTitle(current.name)
-            .setDescription(current.description)
-            .setThumbnail(current.images.mihoyo_icon)
+            .setTitle(data.name)
+            .setDescription(data.description)
+            .setThumbnail(data.images.mihoyo_icon)
             .addFields(
-                { name: 'Weapon', value: `${current.weaponText}`, inline: true },
-                { name: 'Vision', value: `${current?.elementText || '?'}`, inline: true },
-                { name: 'Rarity', value: `${current.rarity}✰`, inline: true },
-                { name: 'Substat', value: `${current.substatText}`, inline: true },
-                { name: 'Region', value: `${current?.region || '?'}`, inline: true },
-                { name: 'Birthday', value: `${current?.birthday || '?'}`, inline: true },
-                { name: 'Material', value: `${current.costs.ascend6[0].name + ', ' + current.costs.ascend6[1].name + ', ' + current.costs.ascend6[2].name + ', ' + current.costs.ascend6[3].name + ', ' + (current.costs.ascend6[4]?.name || '-')}`, inline: true },
+                { name: 'Weapon', value: `${data.weaponText}`, inline: true },
+                { name: 'Vision', value: `${data?.elementText || '?'}`, inline: true },
+                { name: 'Rarity', value: `${data.rarity}✰`, inline: true },
+                { name: 'Substat', value: `${data.substatText}`, inline: true },
+                { name: 'Region', value: `${data?.region || '?'}`, inline: true },
+                { name: 'Birthday', value: `${data?.birthday || '?'}`, inline: true },
+                { name: 'Material', value: `${data.costs.ascend6[0].name + ', ' + data.costs.ascend6[1].name + ', ' + data.costs.ascend6[2].name + ', ' + data.costs.ascend6[3].name + ', ' + (data.costs.ascend6[4]?.name || '-')}`, inline: true },
                 { name: 'Costs', value: `${moracosts}`, inline: true }
             )
             .setFooter({text: `Direquest oleh ${interaction.member.nickname || interaction.user.username}`, iconURL: interaction.user.avatarURL({format : 'png', dynamic : true, size : 1024})})
